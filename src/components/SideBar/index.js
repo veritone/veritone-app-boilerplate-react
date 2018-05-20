f0908b43e9cf26fda3f07b61ed035f69f99708ba
Mitch Robb <mrobb@veritone.com>
2018-05-19 17:25:18 -0700

    add sidebar, topbar

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import {} from 'lodash';
+import { appBarHeight, topBarHeight } from 'veritone-react-common';
+import styles from './styles.scss';
+// import { PropTypes } from 'helpers/react';
+// const {} = PropTypes;
+
+export default class SideBar extends React.Component {
+  static propTypes = {};
+  static defaultProps = {};
+
+  render() {
+    return (
+      <aside
+        className={styles.container}
+        style={{
+          top: appBarHeight + topBarHeight
+        }}
+      />
+    );
+  }
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 // import {  } from 'prop-types';
-import { AppFooter, AppContainer } from 'veritone-react-common';
+import { AppContainer, TopBar } from 'veritone-react-common';
 import { AppBar } from 'veritone-widgets';
+
+import SideBar from 'components/SideBar';
+import sideBarStyles from 'components/SideBar/styles.scss';
 import styles from './styles.scss';
 
 export default class Home extends React.Component {
@@ -11,14 +14,17 @@ export default class Home extends React.Component {
     return (
       <div style={{ height: '100%', backgroundColor: '#FBFBFB' }}>
         <AppBar appSwitcher profileMenu backgroundColor="#2b485c" />
-        <AppContainer appBarOffset appFooterOffset="short">
-          <div className={styles.content}>home content</div>
+        <TopBar appBarOffset />
+        <SideBar />
+        <AppContainer
+          appBarOffset
+          topBarOffset
+          leftOffset={Number(sideBarStyles.sidebarwidth)}
+        >
+          <div className={styles.content}>
+            {'home content test1234'.repeat(1000)}
+          </div>
         </AppContainer>
-        <AppFooter short>
-          <span>© 2017 Veritone, Inc. All Rights Reserved.</span>
-          <a href="#terms">Terms of Use</a>
-          <a href="#privacy">Privacy Policy</a>
-        </AppFooter>
       </div>
     );
   }
