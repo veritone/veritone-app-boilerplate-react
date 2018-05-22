01beff8bf898915b963d398a7ef6c716c0cf58ad
Mitch Robb <mrobb@veritone.com>
2018-05-22 13:25:32 -0700

    add CSSBaseline

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import RootRoute from '../../RootRoute';
-// todo: add global stuff -- snackbar/modal targets, intercom button, ...
 
+// todo: add global stuff -- snackbar/modal targets, intercom button, ...
 const App = () => (
   <div style={{ height: '100%' }}>
+    <CssBaseline />
     <RootRoute />
   </div>
 );
