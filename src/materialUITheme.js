81eac400f14389231efcdf82139df546d80807c4
Mitch Robb <mrobb@veritone.com>
2018-05-24 14:54:01 -0700

    add muiTheme, defualt to blue

---
+++ src/materialUITheme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+export default createMuiTheme({
+  palette: {
+    primary: blue
+  }
+});
--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import RootRoute from '../../RootRoute';
+import materialUITheme from '../../materialUITheme';
 
 // todo: add global stuff -- snackbar/modal targets, intercom button, ...
 const App = () => (
   <div style={{ height: '100%' }}>
     <CssBaseline />
-    <RootRoute />
+    <MuiThemeProvider theme={materialUITheme}>
+      <RootRoute />
+    </MuiThemeProvider>
   </div>
 );
 
