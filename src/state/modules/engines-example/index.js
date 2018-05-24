9a8746cec843de89665ed45afbefbf0a60f0c38f
Mitch Robb <mrobb@veritone.com>
2018-05-24 14:21:15 -0700

    add categories to example module

--- src/state/modules/engines-example/index.js
+++ src/state/modules/engines-example/index.js
@@ -4,18 +4,26 @@ import callGraphQLApi from '~helpers/callGraphQLApi';
 import { without, union } from 'lodash';
 import { set } from 'lodash/fp';
 
-export const FETCH_ENGINE = 'request made to fetch an engine';
+export const FETCH_ENGINE = 'request to fetch an engine';
 export const FETCH_ENGINE_SUCCESS = 'engine fetched successfully';
 export const FETCH_ENGINE_FAILURE = 'engine fetch failed';
+
+export const FETCH_ENGINE_CATEGORIES = `request to fetch the list of engine categories`;
+export const FETCH_ENGINE_CATEGORIES_SUCCESS =
+  'engine categories fetched successfully';
+export const FETCH_ENGINE_CATEGORIES_FAILURE = 'engine categories fetch failed';
+
+// fixme -- use some generic reducer to handle all this boilerplate,
 
 const defaultState = {
   fetchingEngines: [],
   failedFetchingEngines: [],
-  enginesById: {}
+  enginesById: {},
+  fetchingEngineCategories: false,
+  failedFetchingEngineCategories: false,
+  engineCategories: []
 };
 
-// fixme -- use some generic reducer to handle all this boilerplate,
-// with reducereducers
 const reducer = createReducer(defaultState, {
   [FETCH_ENGINE]: (state, action) => ({
     ...state,
@@ -39,6 +47,22 @@ const reducer = createReducer(defaultState, {
     failedFetchingEngines: union(state.fetchingEngines, [
       action.meta.variables.id
     ])
+  }),
+  [FETCH_ENGINE_CATEGORIES]: (state, action) => ({
+    ...state,
+    fetchingEngineCategories: true,
+    failedFetchingEngineCategories: false
+  }),
+  [FETCH_ENGINE_CATEGORIES_SUCCESS]: (state, action) => ({
+    ...state,
+    engineCategories: action.payload.engineCategories.records,
+    fetchingEngineCategories: false,
+    failedFetchingEngineCategories: false
+  }),
+  [FETCH_ENGINE_CATEGORIES_FAILURE]: state => ({
+    ...state,
+    fetchingEngineCategories: false,
+    failedFetchingEngineCategories: true
   })
 });
 
@@ -58,11 +82,44 @@ export const fetchEngine = id => async (dispatch, getState) => {
 
   return await callGraphQLApi({
     actionTypes: [FETCH_ENGINE, FETCH_ENGINE_SUCCESS, FETCH_ENGINE_FAILURE],
-    dispatch,
-    getState,
     query,
     variables: { id },
-    bailout: state => !!selectEngine(state, id)
+    bailout: state => !!selectEngine(state, id),
+    dispatch,
+    getState
+  });
+};
+
+export const fetchEngineCategories = ({ type } = {}) => async (
+  dispatch,
+  getState
+) => {
+  const query = `
+    query($type: String) {
+      engineCategories(type: $type) {
+        records{
+          id
+          name
+          type {
+            name
+            description
+          }
+          iconClass
+        }
+      }
+    }`;
+
+  return await callGraphQLApi({
+    actionTypes: [
+      FETCH_ENGINE_CATEGORIES,
+      FETCH_ENGINE_CATEGORIES_SUCCESS,
+      FETCH_ENGINE_CATEGORIES_FAILURE
+    ],
+    query,
+    variables: { type },
+    dispatch,
+    getState,
+    bailout: (state) => engineCategoriesAreLoading(state)
   });
 };
 
@@ -71,3 +128,9 @@ export const engineIsLoading = (state, id) =>
   local(state).fetchingEngines.includes(id);
 export const engineFailedToFetch = (state, id) =>
   local(state).failedFetchingEngines.includes(id);
+
+export const selectEngineCategories = state => local(state).engineCategories;
+export const engineCategoriesAreLoading = state =>
+  local(state).fetchingEngineCategories;
+export const engineCategoriesFailedToFetch = state =>
+  local(state).failedFetchingEngineCategories;
