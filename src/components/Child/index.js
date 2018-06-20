77c2bdf32e26f1e0920f24423933ab986678dcd1
Andrew Ehlers <asehlers17@gmail.com>
2018-06-19 21:03:32 -0400

    formating and server target changes

---
+++ src/components/Child/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Child = = ({ children }) => (
+  <div className={styles.content}>{children}</div>
+);
+
+export default class Child extends React.Component {
+
+}
