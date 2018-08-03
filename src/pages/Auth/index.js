ac6d8af6aee54732c4d99b91d745844faf7c33f2
Mitch Robb <mrobb@veritone.com>
2018-08-03 16:11:19 -0700

    add basic error messaging to auth page

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -1,22 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { string } from 'prop-types';
 import { OAuthLoginButton } from 'veritone-widgets';
 import { AppContainer } from 'veritone-react-common';
-import { string, shape } from 'prop-types';
-import withAppConfig from '~helpers/withAppConfig';
+import { modules } from 'veritone-redux-common';
+import styles from './styles.scss';
 
-const Auth = ({ appConfig }) => (
+const {
+  config: { getConfig },
+  auth: { selectOAuthError }
+} = modules;
+
+const errorMessages = {
+  unauthorized_client: 'Your account does not have access to this application.',
+};
+
+const genericErrorMessage = error =>
+  `There was an error while trying to authenticate: ${error}.`;
+
+const Auth = ({ OAuthClientID, OAuthErrorCode, OAuthErrorDescription }) => (
   <AppContainer>
-    <OAuthLoginButton
-      clientId={appConfig.OAuthClientID}
-      redirectUri={window.origin}
-    />
+    <div className={styles.container}>
+      {OAuthErrorCode &&
+        (errorMessages[OAuthErrorCode] ||
+          genericErrorMessage(OAuthErrorDescription))}
+      <OAuthLoginButton clientId={OAuthClientID} redirectUri={window.origin} />
+    </div>
   </AppContainer>
 );
 
 Auth.propTypes = {
-  appConfig: shape({
-    OAuthClientID: string.isRequired
-  }).isRequired
+  OAuthClientID: string.isRequired,
+  OAuthErrorCode: string,
+  OAuthErrorDescription: string
 };
 
-export default withAppConfig(Auth);
+export default connect(state => {
+  const error = selectOAuthError(state);
+  return {
+    OAuthClientID: getConfig(state).OAuthClientID,
+    OAuthErrorCode: error.code,
+    OAuthErrorDescription: error.description
+      ? window.unescape(error.description)
+      : null
+  };
+})(Auth);
