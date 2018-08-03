0e2c9c0e11bfb83fa80fbfe0487633fb9e6e6e91
Mitch Robb <mrobb@veritone.com>
2018-08-03 14:16:55 -0700

    add home button to 404 page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Link from 'redux-first-router-link';
 
 import AppContainer from 'components/AppContainer';
 import errorLoading from 'resources/images/error-loading.svg';
 import styles from './styles.scss';
+import { ROUTE_HOME } from '../../state/modules/routing';
 
 export default class NotFound extends React.Component {
   render() {
@@ -19,6 +22,15 @@ export default class NotFound extends React.Component {
               "The page you are looking for has been moved, deleted or doesn't exist."
             }
           </Typography>
+          <Link to={{ type: ROUTE_HOME }}>
+            <Button
+              className={styles.actionButton}
+              variant="raised"
+              color="primary"
+            >
+              Home
+            </Button>
+          </Link>
         </div>
       </AppContainer>
     );
