e6d26865ae12f3e5f84761ce5ba7aee3781f31f0
Mitch Robb <mrobb@veritone.com>
2018-08-30 11:33:06 -0700

    validate that required config exists before booting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import { get, difference } from 'lodash';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
+import { handleImplicitRedirect } from 'veritone-oauth-helpers';
+
 import configureStore from 'state/configureStore';
 
 import 'resources/styles/global.scss';
 
 import App from 'pages/App';
 
-import { handleImplicitRedirect } from 'veritone-oauth-helpers';
+const requiredConfigKeys = [
+  'apiRoot',
+  'switchAppRoute',
+  'loginRoute',
+  'graphQLEndpoint',
+  get(window.config, 'useOAuthGrant') && 'OAuthClientID'
+].filter(Boolean);
+
+const requiredConfigMissing = requiredConfigKeys.some(
+  k => !get(window.config, k)
+);
+
+if (requiredConfigMissing) {
+  const missingKeys = difference(
+    requiredConfigKeys,
+    Object.keys(window.config)
+  );
+
+  throw new Error(
+    `The app requires the keys ${JSON.stringify(
+      requiredConfigKeys
+    )} to exist in config, but the following keys were not found: ${JSON.stringify(
+      missingKeys
+    )}`
+  );
+}
 
 function bootstrap() {
   if (window.name === '_auth') {
